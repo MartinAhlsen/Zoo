39b1b13a8a1f6e7b9cc0a54566b2b6ee1e9d5194
MartinAhlsen <martin.ahlsen@gmail.com>
2022-12-07 13:34:56 +0100

    list fn

--- js/zoo.js
+++ js/zoo.js
@@ -1,10 +1,10 @@
-function Animal(name, group, food, description, length, weight, found, imgSmall, imgLarge) {
+function Animal(name, lifespan, group, food, description, shortDescription, length, weight, found, imgSmall, imgLarge) {
     this.name = name;
-    this.lifespan = this.lifespan;
+    this.lifespan = lifespan;
     this.group = group;
     this.food = food;
     this.description = description;
-    this.shortDescription = this.shortDescription;
+    this.shortDescription = shortDescription;
     this.length = length;
     this.weight = weight;
     this.found = found;
@@ -17,7 +17,7 @@ function Animal(name, group, food, description, length, weight, found, imgSmall,
 let cassowary = new Animal(
                             "Cassowary", 
                             "20 years",
-                            "Bird", 
+                            "bird", 
                             "Plants matter like fruit, insects and small animals like mice amd lizards",
                             "It’s not hard to imagine that cassowaries are descended from dinosaur ancestors. The largest cassowaries can stand as high as six feet and weigh up to 160 pounds.   These large birds cannot fly, but their extremely powerful legs propel them at great speeds. They are strong swimmers and can move quickly on both land and water. Cassowaries are shy and they are usually hard to spot, at least in their natural rain forest habitats. They are not overly aggressive, and attacks are rare. But they can do a lot of damage if they are provoked or angered. Cassowary attacks have occasionally been deadly, including a recent one which occurred in 2019, at a private collection of caged birds in Florida.",
                             "It’s not hard to imagine that cassowaries are descended from dinosaur ancestors. The largest cassowaries can stand as high as six feet and weigh up to 160 pounds.",
@@ -30,7 +30,7 @@ let cassowary = new Animal(
 let echidna = new Animal(
                             "Echidna",
                             "50 years",
-                            "Mammal",
+                            "mammal",
                             "Insects such as ants and termites, beetle larvae and worms",
                             "Echidnas, also called spiny anteaters, are walking contradictions. They are mammals, but they lay eggs. They are often classified as long- or sort-beaked, but don't have beaks at all, in the traditional sense; they have fleshy noses that can be either on the long side or rather short. They don't really look like true anteaters (Myrmecophaga tridactyla), either, and they are not closely related to them. They are spiny, though; their bodies are covered with hollow, barbless quills. Echidnas are monotremes, egg-laying mammals. The only other living monotreme is the platypus.",
                             "Echidnas, also called spiny anteaters, are walking contradictions. They are mammals, but they lay eggs.",
@@ -53,19 +53,55 @@ let frillNeckedLizard = new Animal(
 );
 
 
-let animalArray = ["cassowary"];
+let animalArray = [cassowary, echidna, frillNeckedLizard]; // Lista med alla djur som objekt
 
-
-let generateDivs = console.log("The " + animal.name + " is in the" + animal.group + "group");
-
-function cycleArray() {
-    animalArray.forEach(animal => {
+// function cycleArray() {
+//     animalArray.forEach(animal => {
         
-    });
-}
+//     });
+// }
 
 function logTheAnimals(animal) {
-    console.log("The " + animal.name + " is in the" + animal.group + "group");
+    console.log("The " + animal.name + " is in the " + animal.group + " group");
 }
 
-cycleArray(generateDivs)
+function generateList(animalArray) {
+    if (Array.isArray(animalArray))    
+        animalArray.forEach(element => {    
+            createListItem(element.name);
+        });
+}
+
+function createListItem(animalName) {
+    let createLi = document.createElement("li");
+    createLi.classList.add(removeBlank(animalName), "animal");
+    createLi.setAttribute("id", removeBlank(animalName));
+    createLi.textContent = animalName;
+    document.getElementById("animals").appendChild(createLi);
+}
+
+function removeBlank(stringToModify) {
+    return stringToModify.replaceAll(" ", "-");
+
+    // function generateList(animalArray) {
+//     if (Array.isArray(animalArray))    
+//         animalArray.forEach(element => {    
+//             createListItem(element.name, element.group);
+//         });
+// }
+
+// function createListItem(animalName, targetGroup) {
+//     let createLi = document.createElement("li");
+//     createLi.classList.add(removeBlank(animalName), "animal");
+//     createLi.setAttribute("id", removeBlank(animalName));
+//     createLi.textContent = animalName;
+//     document.getElementById(targetGroup).appendChild(createLi);
+// }
+
+// function removeBlank(stringToModify) {
+//     return stringToModify.replaceAll(" ", "-");
+    
+}
+
+generateList(animalArray)
+logTheAnimals(cassowary);
